9917009fca4a977ef66accecf9644e006320e31e
Owolabi Abdulquawiyy Adisa <98348653+Huygens123@users.noreply.github.com>
2024-05-29 18:54:58 +0100

    Add files via upload

---
+++ Day 8/Day 8 (Function).py
@@ -0,0 +1,24 @@
+# # Review:
+# # Create a function called greet().
+# # Write 3 print statements inside the function.
+# # Call the greet() function and run your code.
+#
+# def greet():
+#     print("How are you doing?")
+#     print("Good morning")
+#     print("Iq am Mat")
+#
+# greet()
+#
+# # Function that allows inputs
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+# greet_with_name("Corrado")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"How is it like in {location}")
+
+greet_with(name="Corradi", location="Osogbo")
