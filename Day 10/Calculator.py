260a4dc691b09e873a8fb38450e1546b84868710
Owolabi Abdulquawiyy Adisa <98348653+Huygens123@users.noreply.github.com>
2024-05-30 23:41:14 +0100

    Add files via upload

---
+++ Day 10/Calculator.py
@@ -0,0 +1,55 @@
+from art import logo
+
+# add
+def add(n1, n2):
+    return n1 + n2
+
+
+# subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+
+# multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+
+# divide
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    '/': divide,
+    '*': multiply,
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+
+    for operator in operations:
+        print(operator)
+    to_continue = True
+
+    while to_continue:
+
+        operations_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        calculation_function = operations[operations_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operations_symbol} {num2} = {answer}")
+
+        should_continue = input(f"Type 'y' to continue calculating with {answer}, "
+                                f"or type 'n to start a new calculation.: ")
+        if should_continue == "y":
+            num1 = answer
+        else:
+            to_continue = False
+            calculator()
+calculator()
